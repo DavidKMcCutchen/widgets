e53d02df25bcb866e3cc17a3ff18a88a0fa325e4
David Kirtland McCutchen <davemccutch@gmail.com>
2023-09-24 02:32:15 -0400

    First commit

--- apps/basic-chart/src/app/app.component.ts
+++ apps/basic-chart/src/app/app.component.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import the BrowserAnimationsModule
+// import { CommonModule } from '@angular/common';
+
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, CalendarModule, FormsModule],
   selector: 'widget-collection-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+
+  // constructor(
+  //   private commonModule: CommonModule,
+  //   private browserAnimationsModule: BrowserAnimationsModule,) {
+  // }
+
   title = 'basic-chart';
+
+  date: Date[] | undefined;
+
 }
